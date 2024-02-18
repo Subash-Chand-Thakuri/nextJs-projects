6036df2ba04403076c9fb4b808645da297331b91
Subash <subashthakuri799@gmail.com>
2024-02-18 16:30:39 +0545

    FullStack Admin Dashboard 1st day

---
+++ nextadmin/app/dashboard/users/page.jsx
@@ -0,0 +1,8 @@
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
---
+++ nextadmin/app/ui/dashboard/sidebar/page.jsx
@@ -0,0 +1,72 @@
+import styles from './sidebar.module.css'
+
+const menuItems = [
+    {
+      title: "Pages",
+      list: [
+        {
+          title: "Dashboard",
+          path: "/dashboard",
+          icon: <MdDashboard />,
+        },
+        {
+          title: "Users",
+          path: "/dashboard/users",
+          icon: <MdSupervisedUserCircle />,
+        },
+        {
+          title: "Products",
+          path: "/dashboard/products",
+          icon: <MdShoppingBag />,
+        },
+        {
+          title: "Transactions",
+          path: "/dashboard/transactions",
+          icon: <MdAttachMoney />,
+        },
+      ],
+    },
+    {
+      title: "Analytics",
+      list: [
+        {
+          title: "Revenue",
+          path: "/dashboard/revenue",
+          icon: <MdWork />,
+        },
+        {
+          title: "Reports",
+          path: "/dashboard/reports",
+          icon: <MdAnalytics />,
+        },
+        {
+          title: "Teams",
+          path: "/dashboard/teams",
+          icon: <MdPeople />,
+        },
+      ],
+    },
+    {
+      title: "User",
+      list: [
+        {
+          title: "Settings",
+          path: "/dashboard/settings",
+          icon: <MdOutlineSettings />,
+        },
+        {
+          title: "Help",
+          path: "/dashboard/help",
+          icon: <MdHelpCenter />,
+        },
+      ],
+    },
+  ];
+  
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>Sidebar</div>
+  )
+}
+
+export default Sidebar
